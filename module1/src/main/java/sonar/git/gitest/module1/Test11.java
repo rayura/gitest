e34e7d0c8d43df55af6f64e75b7c77b8409e7281
yura <yura@nixsolutions.com>
2015-04-16 12:02:15 +0300

    111

--- module1/src/main/java/sonar/git/gitest/module1/Test11.java
+++ module1/src/main/java/sonar/git/gitest/module1/Test11.java
@@ -3,11 +3,6 @@ package sonar.git.gitest.module1;
 public class Test11 {
 	public static void main(String[] args) {
 		try {
-			throw new Exception();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
 			throw new RuntimeException();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -15,7 +10,6 @@ public class Test11 {
 		try {
 			throw new RuntimeException();
 		} catch (Exception e) {
-			e.printStackTrace();
 		}
 	}
 
